621c101e4732f62b1bfaae998edabbdcb8fbafd6
Anthony Ng <newyork.anthonyng@gmail.com>
2016-09-13 14:00:14 -0400

    Add 383 Ransom Note

---
+++ easy/383_Ransom_Note.js
@@ -0,0 +1,30 @@
+/*
+ Given  an  arbitrary  ransom  note  string  and  another  string  containing  letters from  all  the  magazines,  write  a  function  that  will  return  true  if  the  ransom   note  can  be  constructed  from  the  magazines ;  otherwise,  it  will  return  false.   
+
+Each  letter  in  the  magazine  string  can  only  be  used  once  in  your  ransom  note.
+
+Note:
+You may assume that both strings contain only lowercase letters.
+*/
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+	for(var i = 0; i < ransomNote.length; i++) {
+		var currentLetter = ransomNote[i];
+		var magIndex = magazine.indexOf(currentLetter);
+
+		if(magIndex === -1) return false;
+
+		magazine = magazine.slice(0, magIndex) + magazine.slice(magIndex + 1);
+	}
+
+	return true;
+};
+
+console.log(canConstruct('a', 'b'));
+console.log(canConstruct('aa', 'ab'));
+console.log(canConstruct('aa', 'aab'));
