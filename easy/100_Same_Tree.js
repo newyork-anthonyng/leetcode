a5f40c0f843ea6b28be09570b4fca254562acf17
Anthony Ng <newyork.anthonyng@gmail.com>
2016-09-13 14:54:58 -0400

    Add 100 Same Tree

---
+++ easy/100_Same_Tree.js
@@ -0,0 +1,30 @@
+/*
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+	if(p === null && q === null) return true;
+	if(p === null || q === null) return false;
+	if(p.val !== q.val) return false;
+
+	return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+
+//var isSameTree = function(p, q) {
+//	// trivial solution
+//	return JSON.stringify(p) === JSON.stringify(q);
+//}
