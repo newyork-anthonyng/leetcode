edb24090609d17aa04b1e30797a12a16104e8fe9
Anthony Ng <a-anng@expedia.com>
2018-01-16 17:27:46 -0800

    Add answer

--- medium/003_Longest_Substring_Without_Repeating_Characters/index.js
+++ medium/003_Longest_Substring_Without_Repeating_Characters/index.js
@@ -9,7 +9,29 @@
 
  Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
  */
+ 
+ function lengthOfLongestSubstring(s) {
+   let maxLength = 0;
+   let start = -1;
+   const dictionary = {};
+   
+   for (let i = 0; i < s.length; i++) {
+     const currentLetter = s[i];
+     
+     if (typeof dictionary[currentLetter] !== "undefined") {
+       start = Math.max(dictionary[currentLetter], start);
+     }
+     
+     dictionary[currentLetter] = i;
+     maxLength = Math.max(i - start, maxLength);
+   }
+   
+   return maxLength;
+ }
 
+/*
+ * This solution timed out.
+ * The nested for-loop makes it slow
 function lengthOfLongestSubstring(s) {
   let currentWinner = 0;
 
@@ -28,7 +50,13 @@ function lengthOfLongestSubstring(s) {
     }
 
     currentWinner = Math.max(currentCandidate, currentWinner);
+
+    // there is no point going to the end of the list
+    if (currentWinner > (s.length - i)) {
+      break;
+    }
   }
 
   return currentWinner;
 }
+*/
